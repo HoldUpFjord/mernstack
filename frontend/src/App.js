36cd62a2fac2dc06413e74c0cedf347839e4914e
jackmonaco20 <jackmonaco20@yahoo.com>
2022-07-31 20:13:44 -0400

    React integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages & components
 import Home from './pages/Home'
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className = "pages">
           <Routes>
-            <Route>
+            <Route
               path = "/"
-              element = {<Home/>}
-            </Route>
+              element = {<Home />}
+              />
+            
           </Routes>
 
         </div>
