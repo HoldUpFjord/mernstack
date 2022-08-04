52c8c69ff52031e77e8b3e460b677af642cc75ed
jackmonaco20 <jackmonaco20@yahoo.com>
2022-08-04 02:48:13 -0400

    added in update form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,79 @@
+import {useState} from "react"
+
+const WorkoutForm = () => {
+   //create states
+   const [title,setTitle] = useState('')
+   const [load,setLoad] = useState('')
+   const [reps,setReps] = useState('')
+   //error state is set to null by default, as errors don't exist by defualt
+   const [error,setError] = useState(null)
+   
+   const handleSubmit = async (e) =>{
+   //prevents defualt async run: refresh page in this case
+    e.preventDefualt()
+
+    //create object to send as request
+    const workout = {title, load, reps}
+
+    //use fetch api to send post request
+    const response = await fetch('/api/workouts',{
+    method: 'POST',
+    //send workout object to the body of the html as JSOn
+    body: JSON.stringify(workout),
+    headers:{ //
+        'Content-Type': 'applications/json'    }
+    })
+    const json = await response.json()
+    // if an error occurs run setError state which sends json.error
+    if(!response.ok) {
+        setError(json.error)
+        
+    }
+    if(response.ok) {
+        //reset form once request has been sent succesfully
+        setTitle('')
+        setLoad('')
+        setReps('')
+        setError(null)
+        console.log('new workout added')
+    }
+   }
+
+   return (
+    <form className = "create" onSubmit={handleSubmit}>
+        <h3>Add a New Workout</h3>
+
+        <label> Exercise Title:</label>
+        <input
+            type="text"
+            /* e=event: (e.target) is the input field.*/
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+        />
+
+        <label> Load (in kg):</label>
+        <input
+            type="number"
+            /* e=event: (e.target) is the input field.*/
+            onChange={(e) => setLoad(e.target.value)}
+            value={load}
+        />
+
+        <label> Reps:</label>
+        <input
+            type="number"
+            /* e=event: (e.target) is the input field.*/
+            onChange={(e) => setReps(e.target.value)}
+            value={reps}
+        />
+
+        <button>Add Workout</button>
+        
+        
+    </form>
+
+    )
+}
+
+//exports component out of file
+export default WorkoutForm
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import {useEffect , useState} from 'react'
 
 //components
 import WorkoutDetails from '../components/WorkoutDetails'
-
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
@@ -30,6 +30,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
