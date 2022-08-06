603081f1008c6e1cf276f49d7d0d7480b0828caa
jackmonaco20 <jackmonaco20@yahoo.com>
2022-08-05 23:42:45 -0400

    added css for form submission and began working on context

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,12 @@
+import { createContext } from 'react'
+
+export const WorkoutsContext = createContext()
+
+const WorkoutsContextProvider = () => {
+
+    return (
+        <WorkoutsContext.Provider>
+            
+        </WorkoutsContext.Provider>
+    )
+}
